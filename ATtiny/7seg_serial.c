d772d99e51f8e7cd3a9992ce5b3e2b94b9f6c5c9
kal9001 <kal9001@hotmail.com>
2015-02-20 23:49:35 +0000

    update 7seg_serial.c

---
+++ ATtiny/7seg_serial.c
@@ -0,0 +1,20 @@
+/*\
+ *   7seg_serial - v0.1
+ *   Web: http://home.kal9001.co.uk  Email: kal@kal9001.co.uk
+ *   
+ *   Copyright (C) 2015  Kal9001
+ *   
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   called GPLv3.txt in the root directory where this source file is located.
+ *   If not, see <http://www.gnu.org/licenses/>.
+\*/
